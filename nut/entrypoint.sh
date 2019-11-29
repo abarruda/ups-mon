5702da3adcbd1757af7788e98a510e889a341914
Aaron Arruda <aaron@abarruda.com>
2019-11-29 14:23:40 -0800

    config dir, entrypoint cleanup

--- nut/entrypoint.sh
+++ nut/entrypoint.sh
@@ -7,9 +7,6 @@ chown www-data:www-data /usr/lib/cgi-bin/nut/*.cgi
 
 upsdrvctl -D -u root start
 upsd -u root
-sleep 2
-upsc CyberPower-1400VA@localhost
-upsmon 
 
 service fcgiwrap restart
 service nginx restart
--- nut/run.sh
+++ nut/run.sh
@@ -2,13 +2,13 @@ docker run -it \
 	--name ups-mon \
 	--rm \
 	--privileged \
-	-v $(pwd)/ups.conf:/etc/nut/ups.conf \
-	-v $(pwd)/upsd.conf:/etc/nut/upsd.conf \
-	-v $(pwd)/upsd.users:/etc/nut/upsd.users \
-	-v $(pwd)/upsmon.conf:/etc/nut/upsmon.conf \
-	-v $(pwd)/nut.conf:/etc/nut/nut.conf \
-	-v $(pwd)/hosts.conf:/tmp/hosts.conf \
-	-v $(pwd)/nginx.conf:/etc/nginx/sites-available/default \
+	-v $(pwd)/config/ups.conf:/etc/nut/ups.conf \
+	-v $(pwd)/config/upsd.conf:/etc/nut/upsd.conf \
+	-v $(pwd)/config/upsd.users:/etc/nut/upsd.users \
+	-v $(pwd)/config/upsmon.conf:/etc/nut/upsmon.conf \
+	-v $(pwd)/config/nut.conf:/etc/nut/nut.conf \
+	-v $(pwd)/config/hosts.conf:/tmp/hosts.conf \
+	-v $(pwd)/config/nginx.conf:/etc/nginx/sites-available/default \
 	-v $(pwd)/test/CP1500PFCLCD.dev:/etc/nut/CP1500PFCLCD.dev \
 	-p 8888:80 \
 	ups-nut:test
