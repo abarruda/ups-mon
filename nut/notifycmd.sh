1d292f0ea15757f652fa28e53df5a6260b6b96ad
Aaron Arruda <aaron@abarruda.com>
2019-11-30 00:28:59 -0800

    Additional details in email alerts

--- nut/notifycmd.sh
+++ nut/notifycmd.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+battery=`upsc CyberPower-1400VA@localhost battery.charge`
+runtime_seconds=`upsc CyberPower-1400VA@localhost battery.runtime`
+runtime_minutes=$(expr $runtime_seconds / 60)
+load=`upsc CyberPower-1400VA@localhost ups.load`
+
 printf "To: $ALERT_EMAIL_TO\n\
 From: $ALERT_EMAIL_FROM_NAME\ <$ALERT_EMAIL_FROM>\n\
 Subject: $NOTIFYTYPE Battery Alert!\n\n\
+$1\n\n\
+Battery: $battery%%\n\
+Runtime: $runtime_minutes minutes\n\
+Load: $load%%\n\n\
 UPS: $UPSNAME\r\n\
 Alert type: $NOTIFYTYPE" | \
 msmtp -C /.msmtprc \
